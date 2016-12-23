b5874795d839f442db8a09e250c62759daf36414
playerony <playerony@gazeta.pl>
2016-12-23 11:36:39 +0100

    Add cookie to LoginAction.java

--- src/java/com/warehouse/dao/user/LoginAction.java
+++ src/java/com/warehouse/dao/user/LoginAction.java
@@ -6,12 +6,29 @@
 package com.warehouse.dao.user;
 
 import com.warehouse.cookie.SessionManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 
 /**
  *
  * @author pawel_000
  */
-public class LoginAction extends AbstractUserAction{
+public class LoginAction extends AbstractUserAction implements ServletResponseAware, ServletRequestAware{
+    protected HttpServletResponse servletResponse;
+    protected HttpServletRequest servletRequest;
+    
+    @Override
+    public void setServletResponse(HttpServletResponse hsr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setServletRequest(HttpServletRequest hsr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     @Override
     public void validate() {
         if (user.getLogin().length() == (0)) {
@@ -28,8 +45,8 @@ public class LoginAction extends AbstractUserAction{
         if (dao.find(user.getLogin(), user.getPassword())) {
             String rank = dao.getUserRank(user.getLogin(), user.getPassword());
             
-            SessionManager.createCookie("login", user.getLogin());
-            SessionManager.createCookie("rank", rank);
+            servletResponse.addCookie(SessionManager.createCookie("login", user.getLogin()));
+            servletResponse.addCookie(SessionManager.createCookie("rank", rank));
           
             return rank;
         } else {
