1fcba3965c38a1024b1dc6d9e31b6510a2636620
playerony <playerony@gazeta.pl>
2016-12-23 17:48:03 +0100

    Create GlobalInterceptor class

---
+++ src/java/com/warehouse/interceptor/GlobalInterceptor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import java.util.Map;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class GlobalInterceptor implements Interceptor{
+    Map<String, Object> session;
+
+    @Override
+    public void destroy() {
+        
+    }
+
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        session = invocation.getInvocationContext().getSession();
+        
+        return invocation.invoke();
+    }
+    
+}
