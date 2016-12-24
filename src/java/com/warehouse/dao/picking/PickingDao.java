c19aa241ab80577bae085c5a991b8cbaab0e82da
playerony <playerony@gazeta.pl>
2016-12-24 10:23:19 +0100

    Create getPalleteInformations function

--- src/java/com/warehouse/dao/picking/PickingDao.java
+++ src/java/com/warehouse/dao/picking/PickingDao.java
@@ -7,12 +7,18 @@ package com.warehouse.dao.picking;
 
 import com.warehouse.entity.PalleteInfo;
 import com.warehouse.entity.PalletsInMagazine;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.struts2.interceptor.SessionAware;
 
 /**
  *
  * @author pawel_000
  */
-public class PickingDao {
+public class PickingDao implements SessionAware {
+    private Map<String, Object> session;
     private OrderDao orderDao;
     
     public PickingDao(){
@@ -20,10 +26,11 @@ public class PickingDao {
     }
     
     public boolean nextItemButtonAction(PalleteInfo palleteInfo, PalletsInMagazine palletsInMagazine){
-        if(orderDao.checkOrderMaterial(palleteInfo)){
-            
-        }else
-            return false;
+        String result = palleteInfo.getId() + "(" + palleteInfo.getAmount() + ")";
+        
+        String phrase = (String) session.get("items");
+			
+        ArrayList<PalleteInfo> pallete = getPalleteInformations(phrase);
         
         return true;
     }
@@ -32,5 +39,44 @@ public class PickingDao {
         
         
         return true;
+    }
+    
+    public ArrayList<PalleteInfo> getPalleteInformations(String phrase){
+        ArrayList<PalleteInfo> result = new ArrayList<>();
+		
+        if(phrase != null){
+            String id = null;
+            String amount = null;
+
+            Pattern p = Pattern.compile("-?\\d+");
+            Matcher m = p.matcher(phrase);
+            int i = 1;
+
+            while (m.find()) {
+                if(i%2 == 0)
+                    amount = m.group();
+                else
+                    id = m.group();
+
+                i++;
+
+                if(id != null && amount != null){
+                    result.add(new PalleteInfo(Integer.parseInt(id), Integer.parseInt(amount)));
+                    amount = null;
+                    id = null;
+                }
+            }
+         }
+		
+        return result;
+    }
+    
+    public Map<String, Object> getSession() {
+        return session;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> map) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
