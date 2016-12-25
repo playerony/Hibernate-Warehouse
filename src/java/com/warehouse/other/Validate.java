b3a6a233f9b651632cd82909c180b05207fbb06e
playerony <playerony@gazeta.pl>
2016-12-25 15:14:35 +0100

    Add gettPalleteInfo methot in Validate class

--- src/java/com/warehouse/other/Validate.java
+++ src/java/com/warehouse/other/Validate.java
@@ -5,6 +5,11 @@
  */
 package com.warehouse.other;
 
+import com.warehouse.entity.PalleteInfo;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author pawel_000
@@ -35,6 +40,36 @@ public class Validate {
         else
             result = false;
 
+        return result;
+    }
+    
+    public static ArrayList<PalleteInfo> getPalleteInformations(String phrase){
+        ArrayList<PalleteInfo> result = new ArrayList<>();
+		
+        if(phrase != null){
+            String id = null;
+            String amount = null;
+
+            Pattern p = Pattern.compile("-?\\d+");
+            Matcher m = p.matcher(phrase);
+            int i = 1;
+
+            while (m.find()) {
+                if(i%2 == 0)
+                    amount = m.group();
+                else
+                    id = m.group();
+
+                i++;
+
+                if(id != null && amount != null){
+                    result.add(new PalleteInfo(Integer.parseInt(id), Integer.parseInt(amount)));
+                    amount = null;
+                    id = null;
+                }
+            }
+        }
+		
         return result;
     }
 }
