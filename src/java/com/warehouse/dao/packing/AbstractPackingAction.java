d15be35414b38e461df9cfee616aecd7fda3123f
playerony <playerony@gazeta.pl>
2016-12-26 09:42:28 +0100

    Create AbstractPackingAction class

---
+++ src/java/com/warehouse/dao/packing/AbstractPackingAction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.dao.packing;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.warehouse.entity.PalleteInfo;
+import com.warehouse.entity.PalletsPicked;
+
+/**
+ *
+ * @author pawel_000
+ */
+public abstract class AbstractPackingAction extends ActionSupport{
+    protected PalletsPicked palletsPicked;
+    protected PalleteInfo palleteInfo;
+    protected PackingDao packingDao = new PackingDao();
+    
+    @Override
+     public abstract void validate();
+     @Override
+     public abstract String execute();
+
+    public PalletsPicked getPalletsPicked() {
+        return palletsPicked;
+    }
+
+    public void setPalletsPicked(PalletsPicked palletsPicked) {
+        this.palletsPicked = palletsPicked;
+    }
+
+    public PalleteInfo getPalleteInfo() {
+        return palleteInfo;
+    }
+
+    public void setPalleteInfo(PalleteInfo palleteInfo) {
+        this.palleteInfo = palleteInfo;
+    }
+}
