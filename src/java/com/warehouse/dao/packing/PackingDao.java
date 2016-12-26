6e3159bdb27ff9b407ca8cca26fbee6f2f429b92
playerony <playerony@gazeta.pl>
2016-12-26 09:58:04 +0100

    Add PackingDao class and maked getProducts method

---
+++ src/java/com/warehouse/dao/packing/PackingDao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.dao.packing;
+
+import com.warehouse.entity.PalletsPicked;
+import com.warehouse.utility.HibernateUtil;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class PackingDao {
+    public String getProducts(int id){
+        Session session = HibernateUtil.createSessionFactory().openSession();
+        session.beginTransaction();
+        
+        String sql = " from PalletsPicked p where p.id=:id";
+        Query query = session.createQuery(sql);
+        query.setParameter("id", id);
+        List<PalletsPicked> list = query.list();
+        
+        HibernateUtil.shutdown();
+        
+        return list.get(0).getProducts();
+    }
+}
