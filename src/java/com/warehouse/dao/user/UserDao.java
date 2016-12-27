ff95d8de2f0f6106e9ff41cf1563e7ebaa29a69d
playerony <playerony@gazeta.pl>
2016-12-27 14:18:29 +0100

    Create safety methods in UserDao

--- src/java/com/warehouse/dao/user/UserDao.java
+++ src/java/com/warehouse/dao/user/UserDao.java
@@ -8,6 +8,7 @@ package com.warehouse.dao.user;
 import com.warehouse.entity.User;
 import com.warehouse.utility.HibernateUtil;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
@@ -16,84 +17,116 @@ import org.hibernate.Session;
  * @author pawel_000
  */
 class UserDao {
-    public boolean find(String name, String password) {
-        Session session = HibernateUtil.createSessionFactory().openSession();
-        session.beginTransaction();
-        String sql = " from User u where u.login=:name and u.password=:pass";
-        Query query = session.createQuery(sql);
-        query.setParameter("name", name);
-        query.setParameter("pass", password);
-        List<User> list = query.list();
-        
-        if (list.size() > 0) {
+    public boolean find(final String name, final String password) {
+        try{
+            Session session = HibernateUtil.createSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = " from User u where u.login=:name and u.password=:pass";
+            Query query = session.createQuery(sql);
+            query.setParameter("name", name);
+            query.setParameter("pass", password);
+            List<User> list = query.list();
+
+            if (list.size() > 0) {
+                HibernateUtil.shutdown();
+                return true;
+            }
+
             HibernateUtil.shutdown();
-            return true;
+        }catch(HibernateException e){
+            e.printStackTrace();
         }
         
-        HibernateUtil.shutdown();
         return false;
     }
     
-    public boolean addWorker(int id, String firstname, String lastname, String login, String password, String place, String rank){
-        Session session = HibernateUtil.createSessionFactory().openSession();
-        session.beginTransaction();
+    public boolean addWorker(final int id, final String firstname, final String lastname, final String login, 
+                                                   final String password, final String place, final String rank){
+        try{
+            Session session = HibernateUtil.createSessionFactory().openSession();
+            session.beginTransaction();
+            User user = new User(id, firstname, lastname, login, password, place, rank);
+
+            session.save(user);
+            session.getTransaction().commit();
+            HibernateUtil.shutdown();
+            
+            return true;
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
         
-        User user = new User(id, firstname, lastname, login, password, place, rank);
-        
-        session.save(user);
-        session.getTransaction().commit();
-        
-        HibernateUtil.shutdown();
-        
-        return true;
+        return false;
     }
     
-    public boolean deleteWorker(String firstname, String lastname){
+    public boolean deleteWorker(final String firstname, final String lastname){
         boolean result = false;
-        
-        Session session = HibernateUtil.createSessionFactory().openSession();
-        session.beginTransaction();
-        
-        String sql = " delete User where firstname=:name and lastname=:surname";
-        Query query = session.createQuery(sql);
-        query.setParameter("name", firstname);
-        query.setParameter("surname", lastname);
-        int value = query.executeUpdate();
-        
-        if(value==0)
-            result = false;
-        else
-            result = true;
-        
-        session.getTransaction().commit(); 
-        HibernateUtil.shutdown();
+        try{
+            Session session = HibernateUtil.createSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = " delete User where firstname=:name and lastname=:surname";
+            Query query = session.createQuery(sql);
+            query.setParameter("name", firstname);
+            query.setParameter("surname", lastname);
+            int value = query.executeUpdate();
+
+            if(value==0)
+                result = false;
+            else
+                result = true;
+
+            session.getTransaction().commit(); 
+            HibernateUtil.shutdown();
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
         
         return result;
     }
     
-    public String getUserRank(String name, String password){
-        Session session = HibernateUtil.createSessionFactory().openSession();
-        session.beginTransaction();
-        String sql = " from User u where u.login=:name and u.password=:pass";
-        Query query = session.createQuery(sql);
-        query.setParameter("name", name);
-        query.setParameter("pass", password);
-        List<User> list = query.list();
+    public String getUserRank(final String name, final String password){
+        try{
+            Session session = HibernateUtil.createSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = " from User u where u.login=:name and u.password=:pass";
+            Query query = session.createQuery(sql);
+            query.setParameter("name", name);
+            query.setParameter("pass", password);
+            List<User> list = query.list();
+
+            if (list.size() > 0) {
+                HibernateUtil.shutdown();
+                return list.get(0).getRank();
+            }
+
+            HibernateUtil.shutdown();
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
         
-        return list.get(0).getRank();
+        return null;
     }
     
-    public String getUserID(String name, String password){
-        Session session = HibernateUtil.createSessionFactory().openSession();
-        session.beginTransaction();
-        String sql = " from User u where u.login=:name and u.password=:pass";
-        Query query = session.createQuery(sql);
-        query.setParameter("name", name);
-        query.setParameter("pass", password);
-        List<User> list = query.list();
+    public String getUserID(final String name, final String password){
+        try{
+            Session session = HibernateUtil.createSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = " from User u where u.login=:name and u.password=:pass";
+            Query query = session.createQuery(sql);
+            query.setParameter("name", name);
+            query.setParameter("pass", password);
+            List<User> list = query.list();
+
+            if (list.size() > 0) {
+                HibernateUtil.shutdown();
+                return list.get(0).getRank();
+            }
+
+            HibernateUtil.shutdown();
+        }catch(HibernateException e){
+            e.printStackTrace();
+        }
         
-        HibernateUtil.shutdown();
-        
-        return String.valueOf(list.get(0).getId());
+        return "error";
     }
 }
