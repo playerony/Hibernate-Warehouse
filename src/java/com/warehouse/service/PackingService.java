7b1d20f87922c3299e9c166ee9c8069c9dc5d56d
playerony <playerony@gazeta.pl>
2017-02-08 19:50:17 +0100

    Create PackingService class

---
+++ src/java/com/warehouse/service/PackingService.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.service;
+
+import com.warehouse.entity.PalletsPacked;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class PackingService {
+    public static void insert(PalletsPacked palletsPacked, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(palletsPacked);
+        
+        transaction.commit();
+        session.close();
+    }
+    
+    public static void update(PalletsPacked palletsPacked, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(palletsPacked);
+        
+        transaction.commit();
+        session.close();
+    }
+    
+    public static void delete(PalletsPacked palletsPacked, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(palletsPacked);
+        
+        transaction.commit();
+        session.close();
+    }
+    
+    public static List<PalletsPacked> list(SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        List<PalletsPacked> packedList = session.createQuery("from PalletsPacked").list();
+        session.close();
+        
+        return packedList;
+    }
+}
