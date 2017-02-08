0d5be2b01bd7eb3d0093652e460642d11e3b64a8
playerony <playerony@gazeta.pl>
2017-02-08 22:30:09 +0100

    Modify MagazineDao class

--- src/java/com/warehouse/dao/picking/MagazineDao.java
+++ src/java/com/warehouse/dao/picking/MagazineDao.java
@@ -21,89 +21,12 @@ import org.hibernate.Transaction;
  *
  * @author pawel_000
  */
-public class MagazineDao {
+public interface MagazineDao {
+    public boolean checkLocation(String location);
     
-    public boolean checkLocation(final String location){
-        try{
-            Session session = HibernateUtil.createSessionFactory().openSession();
-            session.beginTransaction();
-
-            String sql = " from PalletsInMagazine p where p.location=:locate";
-            Query query = session.createQuery(sql);
-            query.setParameter("locate", location);
-            List<PalletsPicked> list = query.list();
-
-            if(list.size() > 0){
-                HibernateUtil.shutdown();
-                return true;
-            }
-
-            HibernateUtil.shutdown();
-        }catch(HibernateException e){
-            e.printStackTrace();
-        }
-        
-        return false;
-    }
+    public String getProductsByLocation(String location);
     
-    public String getProductsByLocation(final String location){
-        try{
-            Session session = HibernateUtil.createSessionFactory().openSession();
-            session.beginTransaction();
-
-            String sql = " from PalletsInMagazine p where p.location=:locate";
-            Query query = session.createQuery(sql);
-            query.setParameter("locate", location);
-            List<PalletsInMagazine> list = query.list();
-
-            if(list.size() > 0){
-                HibernateUtil.shutdown();
-                return String.valueOf(list.get(0).getProducts());
-            }
-
-            HibernateUtil.shutdown();
-        }catch(HibernateException e){
-            e.printStackTrace();
-        }
-        
-        return "error";
-    }
+    public boolean verifyILocationByItems(String products, PalleteInfo palleteInfo);
     
-    public boolean verifyILocationByItems(final String products, final PalleteInfo palleteInfo){
-        ArrayList<PalleteInfo> palleteItems = Validate.getPalleteInformations(products);
-        for(PalleteInfo p : palleteItems)
-                if(palleteInfo.getId() == p.getId() && palleteInfo.getAmount() <= p.getAmount()){
-                    return true;
-                }
-        
-        return false;
-    }
-    
-    public boolean updateLoctionItems(final String location, final String items){
-        boolean result = false;
-        
-        try{
-            Session session = HibernateUtil.createSessionFactory().openSession();
-            Transaction tx = session.beginTransaction();
-
-            String sql = " update PalletsInMagazine p set p.products=:products where p.location=:location";
-            Query query = session.createQuery(sql);
-            query.setParameter("location", location);
-            query.setParameter("products", items);
-
-            int value = query.executeUpdate();
-            if(value==0)
-                    result = false;
-                else
-                    result = true;
-
-            tx.commit();
-
-            HibernateUtil.shutdown();
-        }catch(HibernateException e){
-            e.printStackTrace();
-        }
-        
-        return result;
-    }
+    public boolean updateLoctionItems(String location, String items);
 }
