869906a94bbff94a85b511b9489f2e9868c14e3e
playerony <playerony@gazeta.pl>
2017-02-09 09:59:57 +0100

    Create PickingDaoImpl

---
+++ src/java/com/warehouse/impl/PickingDaoImpl.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.impl;
+
+import com.warehouse.dao.picking.MagazineDao;
+import com.warehouse.dao.picking.PickingDao;
+import com.warehouse.entity.PalleteInfo;
+import com.warehouse.entity.PalletsInMagazine;
+import com.warehouse.entity.PalletsPicked;
+import com.warehouse.service.PickingService;
+import com.warehouse.utility.Validate;
+import java.util.ArrayList;
+import java.util.Map;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class PickingDaoImpl implements PickingDao{
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public boolean nextItemButtonAction(PalleteInfo palleteInfo, PalletsInMagazine palletsInMagazine, Map<String, Object> session) {
+        MagazineDao magazineDao = new MagazineDaoImpl();
+        boolean res = false;
+        
+        try{
+            String result = palleteInfo.getId() + "(" + palleteInfo.getAmount() + ")";
+            String phrase = (String) session.get("items");
+            String orderPhrase = (String) session.get("order");
+
+            ArrayList<PalleteInfo> palleteItems = Validate.getPalleteInformations(phrase);
+
+            PalleteInfo pal = null;
+            for(PalleteInfo p : palleteItems)
+                if(palleteInfo.getId() == p.getId()){
+                    pal = p;
+                    break;
+                }
+
+            if(pal != null){
+                int value = pal.getAmount() - palleteInfo.getAmount();
+                pal.setAmount(value);
+
+                if(value <= 0)
+                    palleteItems.remove(pal);
+
+                String phr = "";
+                phr = palleteItems.stream().filter((p) -> (p != null)).map((p) -> (p.getId() + "(" + p.getAmount() + ")" + ",")).reduce(phr, String::concat);   
+                session.put("items", phr);
+
+                if(orderPhrase != null)
+                    session.put("order", orderPhrase + "," + result);
+                else
+                    session.put("order", result);
+                
+                String val = magazineDao.getProductsByLocation(palletsInMagazine.getLocation());
+                ArrayList<PalleteInfo> locationItems = Validate.getPalleteInformations(val);
+                phrase = String.valueOf(locationItems.get(0).getId()) + "(" + String.valueOf(locationItems.get(0).getAmount() - palleteInfo.getAmount()) + ")";
+                
+                magazineDao.updateLoctionItems(palletsInMagazine.getLocation(),  phrase);
+
+                res = true;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return res;
+    }
+
+    @Override
+    public boolean createPickingPallete(PalletsPicked palletsPicked) {
+        try{
+            PickingService.insert(palletsPicked, sessionFactory);
+        }catch(Exception e){
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public String getClientID(int orderID) {
+        return String.valueOf(PickingService.list(sessionFactory).get(orderID).getClientID());
+    }
+
+    @Override
+    public boolean updatePickedPallete(int id, String phrase) {
+        try{
+            PalletsPicked palletsPicked = PickingService.list(sessionFactory).get(id);
+            palletsPicked.setProducts(phrase);
+            
+            PickingService.update(palletsPicked, sessionFactory);
+        }catch(Exception e){
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public boolean deletePickedPallete(int id) {
+        try{
+            PickingService.update(PickingService.list(sessionFactory).get(id), sessionFactory);
+        }catch(Exception e){
+            return false;
+        }
+        
+        return true;
+    }
+}
